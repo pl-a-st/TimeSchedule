6506343efdeb2bf1af815d7de8317ea78a7a9c86
Максим Богащенко <pl_a_st@live.ru>
2022-01-07 10:57:15 +0300

    ++

--- time schedule/Holidays.Designer.cs
+++ time schedule/Holidays.Designer.cs
@@ -29,91 +29,232 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnSetListDays = new System.Windows.Forms.Button();
+            this.dTPListDays = new System.Windows.Forms.DateTimePicker();
+            this.tXBListDays = new System.Windows.Forms.TextBox();
+            this.btnSetIntervalDays = new System.Windows.Forms.Button();
+            this.dTPFirstInTheInterval = new System.Windows.Forms.DateTimePicker();
+            this.dTPSecondInTheInterval = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dTPOnceDay = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RemoveDay = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 16);
+            this.listBox1.Location = new System.Drawing.Point(13, 29);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(95, 251);
+            this.listBox1.Size = new System.Drawing.Size(95, 238);
             this.listBox1.TabIndex = 0;
+            // 
+            // btnSetListDays
+            // 
+            this.btnSetListDays.Location = new System.Drawing.Point(142, 64);
+            this.btnSetListDays.Name = "btnSetListDays";
+            this.btnSetListDays.Size = new System.Drawing.Size(282, 27);
+            this.btnSetListDays.TabIndex = 6;
+            this.btnSetListDays.Text = "Отправить";
+            this.btnSetListDays.UseVisualStyleBackColor = true;
+            // 
+            // dTPListDays
+            // 
+            this.dTPListDays.Location = new System.Drawing.Point(6, 68);
+            this.dTPListDays.Name = "dTPListDays";
+            this.dTPListDays.Size = new System.Drawing.Size(130, 20);
+            this.dTPListDays.TabIndex = 5;
+            // 
+            // tXBListDays
+            // 
+            this.tXBListDays.Location = new System.Drawing.Point(6, 38);
+            this.tXBListDays.Name = "tXBListDays";
+            this.tXBListDays.Size = new System.Drawing.Size(354, 20);
+            this.tXBListDays.TabIndex = 4;
+            // 
+            // btnSetIntervalDays
+            // 
+            this.btnSetIntervalDays.Location = new System.Drawing.Point(294, 15);
+            this.btnSetIntervalDays.Name = "btnSetIntervalDays";
+            this.btnSetIntervalDays.Size = new System.Drawing.Size(130, 28);
+            this.btnSetIntervalDays.TabIndex = 9;
+            this.btnSetIntervalDays.Text = "Отправить";
+            this.btnSetIntervalDays.UseVisualStyleBackColor = true;
+            // 
+            // dTPFirstInTheInterval
+            // 
+            this.dTPFirstInTheInterval.Location = new System.Drawing.Point(6, 19);
+            this.dTPFirstInTheInterval.Name = "dTPFirstInTheInterval";
+            this.dTPFirstInTheInterval.Size = new System.Drawing.Size(130, 20);
+            this.dTPFirstInTheInterval.TabIndex = 8;
+            // 
+            // dTPSecondInTheInterval
+            // 
+            this.dTPSecondInTheInterval.Location = new System.Drawing.Point(158, 19);
+            this.dTPSecondInTheInterval.Name = "dTPSecondInTheInterval";
+            this.dTPSecondInTheInterval.Size = new System.Drawing.Size(130, 20);
+            this.dTPSecondInTheInterval.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dTPOnceDay);
+            this.groupBox1.Location = new System.Drawing.Point(114, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 50);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Отправить дату";
+            // 
+            // dTPOnceDay
+            // 
+            this.dTPOnceDay.Location = new System.Drawing.Point(6, 19);
+            this.dTPOnceDay.Name = "dTPOnceDay";
+            this.dTPOnceDay.Size = new System.Drawing.Size(130, 20);
+            this.dTPOnceDay.TabIndex = 12;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.tXBListDays);
+            this.groupBox2.Controls.Add(this.dTPListDays);
+            this.groupBox2.Controls.Add(this.btnSetListDays);
+            this.groupBox2.Location = new System.Drawing.Point(114, 72);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(431, 100);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Отправить список дат";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.dTPFirstInTheInterval);
+            this.groupBox3.Controls.Add(this.btnSetIntervalDays);
+            this.groupBox3.Controls.Add(this.dTPSecondInTheInterval);
+            this.groupBox3.Location = new System.Drawing.Point(114, 178);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(431, 51);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Отправить интервал дат";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Даты через \", \"";
+            // 
+            // RemoveDay
+            // 
+            this.RemoveDay.Location = new System.Drawing.Point(114, 235);
+            this.RemoveDay.Name = "RemoveDay";
+            this.RemoveDay.Size = new System.Drawing.Size(92, 29);
+            this.RemoveDay.TabIndex = 14;
+            this.RemoveDay.Text = "Удалить дату";
+            this.RemoveDay.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Нерабочие дни";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "-";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(250, 67);
+            this.button1.Location = new System.Drawing.Point(366, 35);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "<-";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(114, 67);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(130, 20);
-            this.dateTimePicker1.TabIndex = 5;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(114, 37);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(402, 20);
-            this.textBox1.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(386, 116);
+            this.button2.Location = new System.Drawing.Point(398, 35);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(130, 54);
+            this.button2.Size = new System.Drawing.Size(26, 23);
             this.button2.TabIndex = 9;
-            this.button2.Text = "button2";
+            this.button2.Text = "X";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker2
+            // button3
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(114, 146);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(130, 20);
-            this.dateTimePicker2.TabIndex = 8;
+            this.button3.Location = new System.Drawing.Point(183, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 27);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Применить";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // groupBox4
             // 
-            this.textBox2.Location = new System.Drawing.Point(114, 116);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(266, 20);
-            this.textBox2.TabIndex = 7;
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Location = new System.Drawing.Point(13, 273);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(532, 44);
+            this.groupBox4.TabIndex = 18;
+            this.groupBox4.TabStop = false;
             // 
-            // dateTimePicker3
+            // button4
             // 
-            this.dateTimePicker3.Location = new System.Drawing.Point(250, 146);
-            this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(130, 20);
-            this.dateTimePicker3.TabIndex = 10;
+            this.button4.Location = new System.Drawing.Point(281, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 27);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Отмена";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Holidays
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 279);
-            this.Controls.Add(this.dateTimePicker3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.dateTimePicker2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(562, 335);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RemoveDay);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Holidays";
             this.Text = "Holidays";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,12 +263,24 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSetListDays;
+        private System.Windows.Forms.DateTimePicker dTPListDays;
+        private System.Windows.Forms.TextBox tXBListDays;
+        private System.Windows.Forms.Button btnSetIntervalDays;
+        private System.Windows.Forms.DateTimePicker dTPFirstInTheInterval;
+        private System.Windows.Forms.DateTimePicker dTPSecondInTheInterval;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dTPOnceDay;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button RemoveDay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button4;
     }
 }
