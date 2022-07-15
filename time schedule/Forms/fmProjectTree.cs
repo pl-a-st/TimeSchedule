f1af9ca5e23577fe15586df3901f210b2dd3af6d
Максим Богащенко <sergeyivanov85@yandex.ru>
2022-07-15 16:23:02 +0300

    + форма Дерево проектов

---
+++ time schedule/Forms/fmProjectTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace time_schedule {
+    public partial class fmProjectTree : Form {
+        public fmProjectTree() {
+            InitializeComponent();
+        }
+    }
+}
